3062032fbca5385a1bd26655347c23b082188c57
zl3466 <zl3466@nyu.edu>
2025-12-01 18:41:21 -0500

    updating vision.py

--- nv_train.sh
+++ nv_train.sh
@@ -3,9 +3,14 @@ source /lustre/fsw/portfolios/nvr/users/ymingli/miniconda3/etc/profile.d/conda.s
 conda activate voformer
 export WANDB_API_KEY="de2b136779280d18cb6d59d1a23248b5010833f8"
 
-torchrun --nproc_per_node=4 train.py \
---nusc_data_path /lustre/fsw/portfolios/nvr/users/ymingli/datasets/vlm_visual_odom/NuScenes/train_test \
---batch_size 8 \
---vocab_size 100 \
---wandb_project visual-odometry \
---wandb_run_name nv_vocab100_img196
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node="4" \
+    --nnodes="1" \
+    --node_rank="0" \
+    --master_addr="127.0.0.1" \
+    --master_port="12365" \
+    train.py \
+    --nusc_data_path /lustre/fsw/portfolios/nvr/users/ymingli/datasets/vlm_visual_odom/NuScenes/train_test \
+    --batch_size 8 \
+    --vocab_size 100 \
+    --wandb_project visual-odometry \
+    --wandb_run_name nv_vocab100_img196
